de0513643f9b3dc3c32436668e940f337fbd61fc
Panos-1993 <panoslampros@icloud.com>
2022-07-07 18:10:38 +0300

    Updates

---
+++ precache-manifest.38c21f848652a457d4064fb0dc7cc562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec06d1d7032939e95118",
+    "url": "/static/css/main.9972f7f8.chunk.css"
+  },
+  {
+    "revision": "ec06d1d7032939e95118",
+    "url": "/static/js/main.ec06d1d7.chunk.js"
+  },
+  {
+    "revision": "1116374cc0a1c4e1f2cb",
+    "url": "/static/js/1.1116374c.chunk.js"
+  },
+  {
+    "revision": "79172ea9b939a7d0d28b",
+    "url": "/static/js/2.79172ea9.chunk.js"
+  },
+  {
+    "revision": "d6ba761b1f92348a5cc9",
+    "url": "/static/js/runtime~main.d6ba761b.js"
+  },
+  {
+    "revision": "a9a59fea136e7dbb3a5316b669582d40",
+    "url": "/index.html"
+  }
+];
